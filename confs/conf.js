05a999659eb287138a4fdd844e40d9501ab3f90e
javieryn <javier.yanez@wizeline.com>
2018-09-26 17:46:26 -0500

    Add html reporter
    Add html reporter

--- confs/conf.js
+++ confs/conf.js
@@ -1,6 +1,25 @@
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const SpecReporter = require('jasmine-spec-reporter');
 const env = require('node-env-file');
+const jasmineReporters = require('jasmine-reporters');
+const HTMLReport = require('protractor-html-reporter');
+const xmlMerger = require('junit-report-merger');
+const fs = require('fs');
 env('.env');
+
+function combineReportFiles(exitCode, testConfig) {
+  // Since shardTestFiles = true executes onPrepare() each time - our report files are overwritten each execution.
+  // Logic is to report results from test files each to his own file, and then combine into one XML file,
+  // that is suitable for Jenkins
+  const filesList = fs.readdirSync(process.env.SPECS_REPORTS_PATH);
+  const filesWithPaths = filesList.map(filename => `${process.env.SPECS_REPORTS_PATH}/${filename}`);
+
+
+  xmlMerger.mergeFiles(process.env.JUNIT_TEST_REPORT, filesWithPaths, undefined, (err) => {
+    if (!err) {
+      new HTMLReport().from(process.env.JUNIT_TEST_REPORT, testConfig);
+    }
+  });
+}
 
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -30,16 +49,39 @@ exports.config = {
         defaultTimeoutInterval: 1200000
     },
 
+    plugins: [{
+      package: 'jasmine2-protractor-utils',
+      disableHTMLReport: true,
+      disableScreenshot: false,
+      htmlReportDir: 'testreport/htmlreport',
+      screenshotPath: 'testreport/screenshots',
+      screenshotOnExpectFailure: false,
+      screenshotOnSpecFailure: true,
+      clearFoldersBeforeTest: true,
+    }],
+
     onPrepare: function() {
         browser.ignoreSynchronization = true;
         // Configure jasmine spec reporter.
+        // Configure jasmine spec reporter.
         jasmine.getEnv().clearReporters(); // remove default reporter logs
-        jasmine.getEnv().addReporter(new SpecReporter({ // add jasmine-spec-reporter
+        jasmine.getEnv().addReporter(new SpecReporter.SpecReporter({ // add jasmine-spec-reporter
           spec: {
             displayStacktrace: true,
-            displayPending: true
-          }
+            displayPending: true,
+          },
         }));
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+          consolidateAll: false,
+          savePath: 'testresults',
+          filePrefix: 'xmlresults',
+        }));
+
+        resultLeaker = {
+          suiteStarted: (result) => { jasmine.results = { suite: result }; },
+          specStarted: (result) => { jasmine.results.spec = result; },
+        };
+        jasmine.getEnv().addReporter(resultLeaker);
         // Maximize Window
         setTimeout(function() {
             browser.driver.executeScript(function() {
@@ -51,5 +93,27 @@ exports.config = {
                 browser.driver.manage().window().setSize(result.width, result.height)
             })
         })
-    }
+    },
+
+    onComplete: (errorCode) => {
+    let browserName;
+    let browserVer;
+    const capsPromise = browser.getCapabilities();
+
+    capsPromise.then((caps) => {
+      browserName = caps.get('browserName');
+      browserVer = caps.get('version');
+
+      testConfig = {
+        reportTitle: 'Test Execution Report',
+        outputPath: 'testreport/htmlreport',
+        screenshotPath: '../screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVer,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+      };
+      combineReportFiles(errorCode, testConfig);
+    });
+  },
 }
