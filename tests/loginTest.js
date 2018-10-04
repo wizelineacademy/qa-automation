6954117fe92510742d80c6939b771050ed4d12c9
guimora <43528622+guimora@users.noreply.github.com>
2018-10-03 20:10:18 -0500

    Add files via upload

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -2,7 +2,7 @@ const data = require('../test_data/data');
 const page = require('../page_objects/page')
 const loginPage = require('../page_objects/loginPage');
 const userPage = require('../page_objects/userPage');
-//
+
 beforeAll(function () {
   page.openUrl()
 })
@@ -19,4 +19,5 @@ describe("SignIn into the Site", function() {
         loginPage.positiveTest(data.email, data.firstName, data.lastName, data.emailForm, data.passwordTextField, data.addressFirstName, data.addressLastName, data.addressCompanyName, data.address, data.addressSecondLine, data.addressCity, data.addressZipCode, data.additionalComments, data.homePhone, data.mobilePhone, data.addressAlias);
         expect(userPage.isSignOutBtnDisplayed()).toBe(true);
     });
+    
 });
